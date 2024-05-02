ab46221436c0ef5094e02de9991f84a8ac1d46f7
Phakkhapon Kaewmanee <fishermanfill01@gmail.com>
2024-05-02 13:33:13 +0700

    push

--- Lesson 3/studentcodes/basicjs/nochange.js
+++ Lesson 3/studentcodes/basicjs/nochange.js
@@ -9,6 +9,17 @@ function computeCost() {
 
 // Compute the cost
 
-  document.getElementById("cost").value = 
-  totalCost = xxx
+  var french = 3.49 * document.getElementById("french").value
+  var hazel = 3.95 * document.getElementById("hazel").value
+  var colum = 4.59 * document.getElementById("colum").value
+
+  if(document.getElementById("french").value < 0 
+  || document.getElementById("hazel").value < 0 
+  || document.getElementById("colum").value < 0){
+    alert("Why are you gae?")
+  } else {
+    document.getElementById("cost").value = 
+    totalCost = french + hazel + colum;
+  }
+
 }  //* end of computeCost
