6a78e37ab92b58b48a26fcda32a83c6717031f5b
Phakkhapon Kaewmanee <fishermanfill01@gmail.com>
2024-05-07 10:10:41 +0700

    j

---
+++ Lesson 3/lecture03_js_practice_student/pass.js
@@ -0,0 +1,7 @@
+// pass.js
+//   Register the event handlers for pass.html
+     
+function showInfo(u, p) {
+    // Displaying the username and password in a popup window
+    alert("Username: " + u + "\nPassword: " + p);
+}
