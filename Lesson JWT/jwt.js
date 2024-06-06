e3b40097c37ed335ca645ad1d2234bb0d239fc8d
Phakkhapon Kaewmanee <fishermanfill01@gmail.com>
2024-06-06 23:32:07 +0700

    yes

---
+++ Lesson JWT/jwt.js
@@ -0,0 +1,77 @@
+var express = require('express')
+var app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+var jwt = require('jsonwebtoken');
+     
+app.get('/api', function api(req, res) {
+  res.json({
+    description: 'My API. Please authenticate!'
+  });
+})
+
+app.get('/api/protected', ensureToken, (req, res) => {
+  jwt.verify(req.token, 'secret_key_goes_here', function(err, data) {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        description: 'Protected information. Congrats!',
+        data: data
+      });
+    }
+  });
+})
+
+app.post('/api/login', (req, res) => {
+    const { username, password } = req.body;
+
+    if (username === 'test' && password === '1234') {
+        const user = { id: 3 };
+        const token = jwt.sign({ user: user.id }, 'secret_key_goes_here');
+        res.json({
+            message: "Authenticated! Use this token in the \"Authorization\" header",
+            token: token
+        });
+    } else {
+        res.json({
+            message: 'Invalid Username or Password',
+            user: {
+                username: username,
+                password: password
+            }
+        });
+    }
+});
+
+app.get('/api/userprofile', ensureToken, (req, res) => {
+    jwt.verify(req.token, 'secret_key_goes_here', function(err, data) {
+        if (err) {
+            res.status(403).json({ message: 'forbidden' });
+        } else {
+            res.json({
+                description: `User profile: ${data.username}`,
+                data: {
+                    username: data.username,
+                    iat: data.iat
+                }
+            });
+        }
+    });
+});
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000!');
+})
+
+function ensureToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
